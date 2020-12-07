fc24da3047e475b49c0d999d8f598ecae4f9244e
zzNuAzz <chumeopoco@gmail.com>
2020-12-07 16:37:17 +0700

    get thread details

---
+++ src/graphql-handler/threads/getThread.js
@@ -0,0 +1,53 @@
+const db = require('../../models');
+const { snakeCase, camelCase } = require('change-case-object');
+const {
+    UserInputError,
+    AuthenticationError,
+} = require('apollo-server-express');
+const sequelize = require('sequelize');
+
+const getThread = async (_, args, { userCtx }) => {
+    const { threadId } = args;
+    if (userCtx.error) throw new AuthenticationError(userCtx.error);
+    const {
+        user: { userId, role },
+    } = userCtx;
+
+    const thread = await db.ForumThreads.findOne({
+        where: snakeCase({ threadId }),
+        attributes: { include: [
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM ThreadPosts WHERE ForumThreads.thread_id = ThreadPosts.thread_id)'), 'postCount'
+            ],
+          ]},
+        include: ['author', 'course'],
+        raw: true,
+        nest: true,
+    });
+
+    if(thread === null) {
+        throw new UserInputError("ThreadId is invalid");
+    }
+
+    const course = thread.course;
+
+    if (role === 'Teacher' && course['host_id'] !== userId) {
+        throw new AuthenticationError(
+            'You does not have permission with this course!'
+        );
+    }
+    if (role === 'Student') {
+        const filter = { userId, courseId, status: 'Accepted' };
+        const member = await db.CourseMembers.findOne({
+            where: snakeCase(filter),
+        });
+        if (member == null) {
+            throw new AuthenticationError(
+                'You does not have permission with this course!'
+            );
+        }
+    }
+
+    return camelCase(thread);
+};
+module.exports = getThread;
